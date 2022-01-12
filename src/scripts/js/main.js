57091f8d98f693dc8be2f6f5468e1b7a7aeb53fd
Nathan-santo <nathan.santo12@gmail.com>
2022-01-12 20:22:59 +0100

    Exercice 1

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -1,3 +1,14 @@
 "use strict";
 console.log('Test technique front-end Ubilab');
 //# sourceMappingURL=main.js.map
+
+// MENU RESPONSIVE
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+   }
+   
+   
+function closeNav() {
+     document.getElementById("myNav").style.width = "0%";
+   }
+// MENU RESPONSIVE
